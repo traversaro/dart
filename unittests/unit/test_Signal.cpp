e7a7575b32ceb7fb9b283f15033349bd7a344c8d
Silvio Traversaro <silvio@traversaro.it>
2021-10-10 19:43:21 +0200

    Update test_Signal.cpp

--- unittests/unit/test_Signal.cpp
+++ unittests/unit/test_Signal.cpp
@@ -140,8 +140,8 @@ TEST(Signal, NonStaticMemberFunction)
   signal2.connect(&Viewer::onSignal2Static);
 
   // Connect non-static member function
-  using std::placeholders::_1;
-  using std::placeholders::_2;
+  using placeholders::_1;
+  using placeholders::_2;
   signal1.connect(bind(&Viewer::onSignal1, &viewer, _1));
   signal2.connect(bind(&Viewer::onSignal2, &viewer, _1, _2));
 
